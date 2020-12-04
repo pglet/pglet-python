2c8427d8462fe7ff2fab30f911b58de40cfea76a
Feodor Fitsner <feodor@appveyor.com>
2020-12-04 09:31:05 -0800

    PGLET_VERSION = "0.1.8"

--- pglet/pglet.py
+++ pglet/pglet.py
@@ -12,7 +12,7 @@ from .utils import is_windows, which
 from .textbox import Textbox
 from .connection import Connection
 
-PGLET_VERSION = "0.1.7"
+PGLET_VERSION = "0.1.8"
 pglet_exe = ""
 
 def page(name='', web=False, private=False, server='', token='', no_window=False):
