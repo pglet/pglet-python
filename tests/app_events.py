7df3f6fb9a2011ae18ec1d9ff6e89c76fc8b08d1
Feodor Fitsner <feodor@appveyor.com>
2020-12-06 19:51:39 -0800

    Test app into tests

---
+++ tests/app_events.py
@@ -0,0 +1,37 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+import time
+import pglet
+from pglet import Text, Textbox, Button, Progress
+
+page = pglet.page("index")
+page.clean()
+
+txt = Textbox(label="Your name")
+btn = Button(text="Say hello", primary=True)
+page.add(txt, btn)
+
+while True:
+    e = page.wait_event()
+
+    if e.target == btn.id and e.name == "clicked":
+        name = page.get_value(txt)
+
+        if name == "":
+            txt.error_message = "Name cannot be blank"
+            btn.text = "Say it again!"
+            page.update(txt, btn)
+            continue
+
+        page.remove(btn)
+        page.add(Text(value=f'Hello, {name}!'), at=0)
+
+        prg = page.add(Progress(label="Doing something..."))
+        for i in range(1, 11):
+            page.set_value(prg, i * 10)
+            time.sleep(1)
+
+        break
