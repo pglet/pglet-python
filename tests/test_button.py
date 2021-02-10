6ac688d9518b272de48ca4c8ef6a1898edd00f1c
Inesa Fitsner <inesa@appveyor.com>
2021-02-10 10:30:46 -0800

    added data property to Button test

--- tests/test_button.py
+++ tests/test_button.py
@@ -7,7 +7,7 @@ def test_button_primary_must_be_bool():
         Button(id="button1", text="My button", primary="1")
 
 def test_button_add():
-    b = Button(id="button1", text="My button")
+    b = Button(id="button1", text="My button", primary=True, data='this is data')
     assert isinstance(b, pglet.Control)
     assert isinstance(b, pglet.Button)
-    assert b.get_cmd_str() == ('button id="button1" text="My button"'), "Test failed"
+    assert b.get_cmd_str() == ('button id="button1" data="this is data" primary="true" text="My button"'), "Test failed"
