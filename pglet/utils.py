59aedb3e5efc016fca2883ab5b52ae2ff13bd42a
Inesa Fitsner <inesa@appveyor.com>
2021-02-16 13:24:22 -0800

    Update utils.py
    fixed \ in string

--- pglet/utils.py
+++ pglet/utils.py
@@ -3,7 +3,7 @@ import re
 
 def encode_attr(attr):
     attr = str(attr)
-    return attr.replace("\n", "\\n").replace("\"", "\\\"")
+    return attr.replace("\\", "\\\\").replace("\n", "\\n").replace("\"", "\\\"")
 
 def is_windows():
     return platform.system() == "Windows"
