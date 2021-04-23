6bef7088595b47180b0d408056c87e037ef83c84
Feodor Fitsner <feodor@appveyor.com>
2021-04-23 11:34:47 -0700

    Vertical BarChart example

--- tests/test_verticalbarchart.py
+++ tests/test_verticalbarchart.py
@@ -1,15 +1,15 @@
 import pglet
 from pglet import VerticalBarChart
-from pglet.verticalbarchart import P
+from pglet.verticalbarchart import Point
 
 
 def test_verticalbarchart_add():
     vbc = VerticalBarChart(legend=True, tooltips=False, bar_width=56, 
         colors='green yellow', y_min=0, y_max=1000, y_ticks=200, y_format='format{y}',
         x_type='number', points=[
-        P(x='1', y=100, legend='legend', color='green', x_tooltip='x tooltip', y_tooltip='y tooltip'),
-        P(x='80', y=200), 
-        P(x='100', y=300),
+        Point(x='1', y=100, legend='legend', color='green', x_tooltip='x tooltip', y_tooltip='y tooltip'),
+        Point(x='80', y=200), 
+        Point(x='100', y=300),
     ])
     assert isinstance(vbc, pglet.Control)
     assert isinstance(vbc, pglet.VerticalBarChart)
