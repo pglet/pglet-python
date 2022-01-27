40ca384ffe9df3fceab03a2c2b7b892bc3bda450
mikaelho <mikael.honkala@gmail.com>
2022-01-27 05:25:36 +0200

    Minor fixes to examples to work with PR #56 (#58)

--- examples/hello-app.py
+++ examples/hello-app.py
@@ -2,6 +2,6 @@ import pglet
 from pglet import Text
 
 def main(page):
-    page.add(Text(f"Hello to session {page.connection.conn_id}!"))
+    page.add(Text(f"Hello to session {page.connection.host_client_id}!"))
 
 pglet.app(target=main, web=True)
