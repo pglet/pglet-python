8f9bbd6ad1e2c88bc2df1ec2c81f79c37136baf0
Mikael Honkala <mikael.honkala@gmail.com>
2022-02-13 09:55:21 +0200

    Test fixes: Remove obsolete border parameters

--- tests/test_text.py
+++ tests/test_text.py
@@ -17,28 +17,42 @@ def test_text_add():
         block=False,
         color="#9FE2BF",
         bgcolor="#FF7F50",
-        border="1px solid #550000",
         border_style="dotted",
         border_width="1",
         border_color="yellow",
         border_radius="4px",
-        border_left="1px solid #550000",
-        border_right="1px solid #550000",
-        border_top="1px solid #550000",
-        border_bottom="1px solid #550000",
     )
     assert isinstance(c, pglet.Control)
     assert isinstance(c, pglet.Text)
     # raise Exception(s.get_cmd_str())
     # assert c.get_cmd_str() == ('text align="left" block="false" bold='true italic="false" nowrap="true" pre="false" size="tiny" value="Hello,\\nworld!" verticalAlign="left"'), "Test failed"
-    assert c.get_cmd_str() == (
-        'text align="left" bgcolor="#FF7F50" block="false" bold="true" '
-        'border="1px solid #550000" borderbottom="1px solid #550000" bordercolor="yellow" '
-        'borderleft="1px solid #550000" borderradius="4px" borderright="1px solid #550000" '
-        'borderstyle="dotted" bordertop="1px solid #550000" borderwidth="1" color="#9FE2BF" '
-        'italic="false" markdown="true" nowrap="true" pre="false" size="tiny" value="Hello,\\nworld!" '
-        'verticalalign="top"'
-    ), "Test failed"
+    assert c.get_cmd_str() == [
+        Command(
+            indent=0,
+            name=None,
+            values=["text"],
+            attrs={
+                "align": "left",
+                "bgcolor": "#FF7F50",
+                "block": "false",
+                "bold": "true",
+                "bordercolor": "yellow",
+                "borderradius": "4px",
+                "borderstyle": "dotted",
+                "borderwidth": "1",
+                "color": "#9FE2BF",
+                "italic": "false",
+                "markdown": "true",
+                "nowrap": "true",
+                "pre": "false",
+                "size": "tiny",
+                "value": "Hello,\nworld!",
+                "verticalalign": "top",
+            },
+            lines=[],
+            commands=[],
+        )
+    ], "Test failed"
 
 
 def test_text_double_quotes():
@@ -50,10 +64,19 @@ def test_text_double_quotes():
 
 
 def test_add_text_inside_stack():
-    txt = Text(id="txt1", value='Hello, "world!"')
-    btn = Button(text="Super button")
-    stack = Stack(id="header", controls=[txt, btn])
+    text = Text(id="txt1", value='Hello, "world!"')
+    button = Button(text="Super button")
+    stack = Stack(id="header", controls=[text, button])
 
-    # open page
-    p = pglet.page("test_text", local=True, no_window=True)
-    p.clean()
+    assert stack.get_cmd_str() == [
+        Command(indent=0, name=None, values=["stack"], attrs={"id": ("header", True)}, lines=[], commands=[]),
+        Command(
+            indent=2,
+            name=None,
+            values=["text"],
+            attrs={"value": 'Hello, "world!"', "id": ("txt1", True)},
+            lines=[],
+            commands=[],
+        ),
+        Command(indent=2, name=None, values=["button"], attrs={"text": "Super button"}, lines=[], commands=[]),
+    ], "Test failed"
